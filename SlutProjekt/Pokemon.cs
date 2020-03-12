c7e8b47741f1080050bd57199f77d858176b46e4
AxelP312 <axepetaxe@gmail.com>
2020-03-12 10:44:29 +0100

    Add project files.

---
+++ SlutProjekt/Pokemon.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SlutProjekt
+{
+    class Pokemon
+    {
+        public string name;
+        public int health;
+        public int damage;
+        public int speed;
+        public int defense;
+        
+        /*Funktion som tar in parametrar från Program.cs och definerar sedan vad för saker pokemonen har.
+         För att anropa den här metoden gör man */
+        public Pokemon(string n, int hp, int dmg, int spd, int def)
+        {
+            name = n;
+            damage = dmg;
+            health = hp;
+            speed = spd;
+            defense = def;
+        }
+    }
+}
---
+++ SlutProjekt/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SlutProjekt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Här anropar jag Pokemon.cs genom att lägga in parametrar i funktionen Pokemon(). 
+            //Namn, Hp, Dmg, Spd, def
+            //För att se t.ex. Magikarps attack skriver man "Magikarp.Damage".
+            Pokemon Magikarp = new Pokemon("Magikarp", 50, 5, 80, 20);
+            Pokemon Ditto = new Pokemon("Ditto", 48, 48, 48, 48);
+
+        }
+    }
+}
